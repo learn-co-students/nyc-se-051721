cbaa84390f2e5d8de910c6ac2b570be7ee1c7dc5
Dwire <gjdwyer8@gmail.com>
2021-07-23 18:14:22 -0400

    review

--- rails_review/app/controllers/users_controller.rb
+++ rails_review/app/controllers/users_controller.rb
@@ -15,21 +15,14 @@ class UsersController < ApplicationController
 
   # # POST /users
   def create
-    user = User.new 
-    user.name = params[:user][:name]
-    user.age = params[:user][:age]
-    user.email = params[:user][:email]
 
+    @user = User.new(user_params)
 
-    user.save
-
-    # @user = User.create(user_params)
-
-    # if @user.save
-    #   render json: @user, status: :created, location: @user
-    # else
-    #   render json: @user.errors, status: :unprocessable_entity
-    # end
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   # # PATCH/PUT /users/1
--- rails_review/app/models/user.rb
+++ rails_review/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+
+  validates :email, uniqueness: true
+  validates :name, uniqueness: true
 end
